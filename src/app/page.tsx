727a156e6cedd6346361ea013cbf0fc185bea097
Ziyad-Beyg <CG05@CG-21.ssuet.edu.pk>
2023-07-10 20:55:00 +0500

    Navbar Done

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import  Navbar  from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      {/* <Button variant="outline" className="">Button</Button> */}
+    </div>
+  );
+}
